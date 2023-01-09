f3f68be8826ae904bd2a2451a0fe98346d154372
Osama Rais <osamaraees98@hotmail.com>
2023-01-09 16:05:36 +0500

    first commit

---
+++ src/components/SimpleInput.js
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+
+const SimpleInput = (props) => {
+   const nameInputRef = useRef();
+   const [enteredName, setEnteredName] = useState("");
+
+   const nameInputHandler = (event) => {
+      setEnteredName(event.target.value);
+   };
+
+   const formSubmitHandler = (event) => {
+      event.preventDefault();
+
+      console.log(enteredName);
+
+      const enterdValue = nameInputRef.current.value;
+      console.log(enterdValue);
+   };
+   return (
+      <form onSubmit={formSubmitHandler}>
+         <div className="form-control">
+            <label htmlFor="name">Your Name</label>
+            <input
+               ref={nameInputRef}
+               type="text"
+               id="name"
+               onChange={nameInputHandler}
+            />
+         </div>
+         <div className="form-actions">
+            <button>Submit</button>
+         </div>
+      </form>
+   );
+};
+
+export default SimpleInput;
